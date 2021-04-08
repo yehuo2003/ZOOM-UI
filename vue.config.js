0ba1387761de63a4794ce7ae1d4e59af5dcf774d
林主明 <9208608@qq.com>
2021-04-08 21:08:34 +0800

    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,14 @@
  * @Autor: linzhuming
  * @Date: 2021-03-28 10:02:14
  * @LastEditors: linzhuming
- * @LastEditTime: 2021-04-08 20:58:55
+ * @LastEditTime: 2021-04-08 21:07:28
  */
 // 生产环境
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   // publicPath: 'http://yehuo2003.gitee.io/zoom-ui-direction/',
   // publicPath: isProduction ? 'zoom-ui-direction/' : '/',
-  publicPath: './',
+  publicPath: isProduction ? './' : '/',
   // outputDir: 'dist',
   outputDir: 'docs',
   productionSourceMap: false,
